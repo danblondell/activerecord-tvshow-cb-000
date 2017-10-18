5d363eeb51f965b6748061f3707693c8c3daf212
Dan Blondell <danblondell@gmail.com>
2017-10-18 01:35:17 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -21,12 +21,12 @@ class Show < ActiveRecord::Base
   end
 
   def self.popular_shows
-    self.where(:rating > 5)
+    self.where(:rating > 5).name
   end
 
 
   def self.shows_by_alphabetical_order
-
+    self.order(:name, ASC)
   end
 
 end
